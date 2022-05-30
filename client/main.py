2840d9d133533454e68daee645bc1b1bb8b8e659
Lucas-WF <lucas.wf@outlook.com>
2022-05-30 10:55:37 -0300

    Add login, about and modify homepage

--- client/main.py
+++ client/main.py
@@ -3,9 +3,17 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def main_page():
+@app.route("/")
+def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
