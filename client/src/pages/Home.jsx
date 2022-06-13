591db9de92b2397c28a3ee3bae549fed5c7e0523
Lucas-WF <lucas.wf@outlook.com>
2022-06-13 18:18:11 -0300

    Add uploaded_images folder and refact homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,19 @@ import React from "react";
 import "../assets/css/MarginTop.css"
 
 export default function Home() {
-    return (
-        <div className="home">
-            <h1 class="text-center mt-5">Bem-vindo ao nosso Ateliê!</h1>
-            
-        </div>
-    );
+    if (localStorage.getItem('token')) {
+        return (
+            <div className="home">
+                <h1 class="text-center mt-5">Bem-vindo ao nosso Ateliê!</h1>
+
+            </div>
+        );
+
+    } else {
+        return (
+            <div className="home">
+                <h1 class="text-center mt-5">Bem-vindo ao nosso Ateliê!</h1>
+            </div>
+        );
+    }
 }
